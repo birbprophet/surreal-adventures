3ef4262b6fcb14ae175294639df93a88b3b87347
Benjamin Tang <benjamin.tang.hz@gmail.com>
2019-12-17 17:00:57 +0800

    added firebase.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,23 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import PageWrapper from "../components/core/PageWrapper"
-import Image from "../components/images/AstronautImage"
+import AstronautImage from "../components/images/AstronautImage"
 import SEO from "../components/core/seo"
+
+const HomePage: React.FC<IProps> = ({ data }) => (
+  <PageWrapper>
+    <SEO title="Home" />
+    <h1>{data.datoCmsHomePage.title}</h1>
+    <p>{data.datoCmsHomePage.description}</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <AstronautImage />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </PageWrapper>
+)
+
+export default HomePage
 
 export interface IProps {
   data: IData
@@ -17,21 +32,6 @@ export interface IDatoCmsHomePage {
   description: string
   title: string
 }
-
-const HomePage: React.FC<IProps> = ({ data }) => (
-  <PageWrapper>
-    <SEO title="Home" />
-    <h1>{data.datoCmsHomePage.title}</h1>
-    <p>{data.datoCmsHomePage.description}</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </PageWrapper>
-)
-
-export default HomePage
 
 export const query = graphql`
   query HomePageQuery {
