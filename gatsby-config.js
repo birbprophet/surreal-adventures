020d0905e42826d7de624e0f9b28c7856208d608
Benjamin Tang <benjamin.tang.hz@gmail.com>
2019-12-17 11:31:51 +0800

    added gatsby client paths plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,23 +35,9 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-
     {
-      resolve: "gatsby-theme-firebase",
-      options: {
-        credentials: {
-          apiKey: process.env.FIREBASE_API_KEY,
-          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-          databaseURL: process.env.FIREBASE_DATABASE_URL,
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.FIREBASE_APP_ID,
-        },
-        loginPath: "/login",
-        loginRedirectPath: "/",
-        socialLogins: ["google", "facebook"],
-      },
+      resolve: `gatsby-plugin-create-client-paths`,
+      options: { prefixes: [`/app/*`] },
     },
   ],
 }
