a747a4e0c5842726ff9702d059846a2d48ded0d3
Benjamin Tang <benjamin.tang.hz@gmail.com>
2019-12-19 16:30:56 +0800

    basic menu items

--- src/components/layouts/elements/MenuItems.tsx
+++ src/components/layouts/elements/MenuItems.tsx
@@ -1,21 +1,71 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 
 import ChevronUpSvg from "../../../assets/svgs/cheveron-up.svg"
+import ChevronDownSvg from "../../../assets/svgs/cheveron-down.svg"
 
 const ReactComponent: React.FC = () => {
+  const [resourcesOpen, setResourcesOpen] = useState(true)
+
+  const handleResourcesOnClick = () => {
+    setResourcesOpen(!resourcesOpen)
+  }
   return (
-    <div className="absolute top-0 left-0 mt-20 mx-6 w-9/12">
-      <Link to="/" className="text-xl text-gray-200">
-        What is Surreal Adventures?
-      </Link>
-      <hr className="mb-3" />
-      <Link to="/" className="text-xl text-gray-200 flex">
-        <div>Resources</div>
-        <ChevronUpSvg className="fill-current w-4 h-4 mr-2" />
-      </Link>
-      <hr className="" />
-    </div>
+    <>
+      <div className="absolute top-0 left-0 mt-20 mx-6 w-9/12 flex flex-col">
+        <Link to="/">
+          <div className="pb-2 border-b-2 border-gray-600">
+            <div className="text-xl text-gray-200 pl-4 pr-2 py-2">
+              How It Works
+            </div>
+          </div>
+        </Link>
+        <div className="pb-2 border-b-2 border-gray-600 mt-4">
+          <div
+            className={
+              "text-xl text-gray-200 flex rounded-lg pl-4 pr-2 py-2 " +
+              (resourcesOpen ? "bg-gray-700" : "")
+            }
+            onClick={handleResourcesOnClick}
+          >
+            <div className="flex-1">Resources</div>
+            <div>
+              {resourcesOpen ? (
+                <ChevronUpSvg className="fill-current w-8 h-8" />
+              ) : (
+                <ChevronDownSvg className="fill-current w-8 h-8" />
+              )}
+            </div>
+          </div>
+          <div className={"ml-6 my-2 " + (resourcesOpen ? "" : "hidden")}>
+            <div className="mb-2">
+              <Link to="/" className="text-xl text-gray-200">
+                Getting Started
+              </Link>
+            </div>
+            <div className="mb-2">
+              <Link to="/" className="text-xl text-gray-200">
+                Generating Stories
+              </Link>
+            </div>
+            <div className="mb-2">
+              <Link to="/" className="text-xl text-gray-200">
+                Playing With Friends
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <Link to="/">
+          <div className="pb-2 border-b-2 border-gray-600 mt-4">
+            <div className="text-xl text-gray-200 pl-4 pr-2 py-2">FAQs</div>
+          </div>
+        </Link>
+      </div>
+      <div className="absolute bottom-0 left-0 bg-gray-100 m-6">
+        Bottom Section
+      </div>
+    </>
   )
 }
 
