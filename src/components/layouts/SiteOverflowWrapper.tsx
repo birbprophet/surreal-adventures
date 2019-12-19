b3827bc1e824d0dfaa863a56209df28d47de1bb0
Benjamin Tang <benjamin.tang.hz@gmail.com>
2019-12-19 17:33:49 +0800

    added overflow-x-hidden

--- src/components/layouts/SiteOverflowWrapper.tsx
+++ src/components/layouts/SiteOverflowWrapper.tsx
@@ -8,7 +8,11 @@ const ReactComponent: React.FC<IProps> = ({ children }) => {
   let { siteState } = SiteState.useContainer()
   const { menuOpen } = siteState
 
-  return <div className={menuOpen ? "overflow-hidden" : ""}>{children}</div>
+  return (
+    <div className={"overflow-x-hidden" + menuOpen ? "overflow-y-hidden" : ""}>
+      {children}
+    </div>
+  )
 }
 
 export default ReactComponent
