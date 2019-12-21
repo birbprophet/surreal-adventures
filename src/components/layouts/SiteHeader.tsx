8bbc37015287cbe0b22dca4147f2a332f1deff40
Benjamin Tang <benjamin.tang.hz@gmail.com>
2019-12-21 13:11:01 +0800

    another fix for header

--- src/components/layouts/SiteHeader.tsx
+++ src/components/layouts/SiteHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react"
+import React from "react"
 import { Link } from "gatsby"
 
 import Typist from "react-typist"
@@ -17,31 +17,31 @@ const ReactComponent: React.FC = () => {
 
   return (
     <>
-      <div className="fixed top-0 z-20">
-        <div className="left-0 flex pt-2">
-          <div className="my-2 mx-6 cursor-pointer relative ">
-            <Link
-              to="/"
-              className="font-rounded font-semibold text-gray-100 text-2xl"
+      <div className="fixed top-0 z-20 left-0 flex pt-2 w-full bg-gray-900 pb-3">
+        <div className="my-2 mx-6 cursor-pointer">
+          <Link
+            to="/"
+            className="font-rounded font-semibold text-gray-100 text-2xl"
+          >
+            <Typist
+              avgTypingDelay={100}
+              startDelay={1000}
+              cursor={{
+                show: true,
+                blink: true,
+                element: "_",
+                hideWhenDone: false,
+              }}
             >
-              <Typist
-                avgTypingDelay={100}
-                startDelay={1000}
-                cursor={{
-                  show: true,
-                  blink: true,
-                  element: "_",
-                  hideWhenDone: false,
-                }}
-              >
-                Surreal <Typist.Delay ms={500} />
-                Adventures
-              </Typist>
-            </Link>
-          </div>
-          <MenuOverlay menuOpen={menuOpen} />
-          <MenuIcon menuOpen={menuOpen} toggleMenuOpen={toggleMenuOpen} />
+              Surreal <Typist.Delay ms={500} />
+              Adventures
+            </Typist>
+          </Link>
         </div>
+        <MenuOverlay menuOpen={menuOpen} />
+      </div>
+      <div className="fixed top-0 z-20 left-0 flex pt-2 bg-gray-900">
+        <MenuIcon menuOpen={menuOpen} toggleMenuOpen={toggleMenuOpen} />
       </div>
     </>
   )
