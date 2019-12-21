8144e12a4938824a16f25adb2104c5dc5173e5cb
Benjamin Tang <benjamin.tang.hz@gmail.com>
2019-12-21 15:31:25 +0800

    changed order in layout

---
+++ src/components/core/StyleWrapper.tsx
@@ -0,0 +1,18 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+
+const Component: React.FC = ({ children }) => {
+  return (
+    <>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default Component
